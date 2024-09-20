e8716d1ac25341522159fe8f920fc83780331e70
NaynaBisht1 <145502271+NaynaBisht@users.noreply.github.com>
2024-09-21 05:11:29 +0530

    infosec

--- bitescout/src/components/Header.js
+++ bitescout/src/components/Header.js
@@ -1,14 +1,14 @@
-const Header = () =>{
-    return(
-        <nav className="bg-orange-300 p-3 flex items-center justify-between rounded">
-            <ul className=" list-none flex gap-5 justify-between">
-                <li>Discover</li>
-                <li>Contribute</li>
+const Header = () => {
+    return (
+        <nav className="bg-orange-300 p-3 flex items-center justify-between rounded flex-wrap">
+            <ul className="list-none flex gap-5 order-1">
+                <li>Blog</li>
                 <li>Add Product</li>
             </ul>
-            <h1 className=" font-bold text-3xl ml-36 lg:ml-60">BiteScout</h1>
-            <button className=" ml-auto  bg-rose-500  text-white px-4 py-2 rounded ">SignIn</button>
+            <h1 className="font-bold text-3xl mx-auto order-2">BiteScout</h1>
+            <button className="bg-cyan-600 text-white px-4 py-2 rounded order-3">SignIn</button>
         </nav>
-    )
-}
-export default Header
+    );
+};
+
+export default Header;
